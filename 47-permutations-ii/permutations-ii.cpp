241c09f4721f215b51b8f64c9291d93ed996d99a
Allah Wasaya <136077480+wasaya33@users.noreply.github.com>
2025-10-09 21:19:36 +0500

    Time: 0 ms (100.00%) | Memory: 11.2 MB (97.33%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+        vector<int> path;
+        vector<bool> used(nums.size(), false);
+        dfs(nums, used, path, res);
+        return res;
+    }
+    
+private:
+    void dfs(const vector<int>& nums, vector<bool>& used, vector<int>& path, vector<vector<int>>& res) {
+        if (path.size() == nums.size()) {
+            res.push_back(path);
+            return;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            if (used[i]) continue;
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+            used[i] = true;
+            path.push_back(nums[i]);
+            dfs(nums, used, path, res);
+            path.pop_back();
+            used[i] = false;
+        }
+    }
+};
