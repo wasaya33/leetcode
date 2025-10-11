ae0020f5c105df59814788b3ca171d208dcf877f
Allah Wasaya <136077480+wasaya33@users.noreply.github.com>
2025-10-11 10:06:44 +0500

    Time: 0 ms (100.00%) | Memory: 9.3 MB (57.86%) - LeetSync

---
+++ 97-interleaving-string/interleaving-string.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int len1 = s1.length();
+        int len2 = s2.length();
+        int len3 = s3.length();
+
+        if (len1 + len2 != len3) {
+            return false;
+        }
+
+        vector<vector<bool>> dp(len1 + 1, vector<bool>(len2 + 1, false));
+        dp[0][0] = true;
+
+        for (int i = 1; i <= len1; ++i) {
+            if (s1[i - 1] == s3[i - 1] && dp[i - 1][0]) {
+                dp[i][0] = true;
+            }
+        }
+
+        for (int j = 1; j <= len2; ++j) {
+            if (s2[j - 1] == s3[j - 1] && dp[0][j - 1]) {
+                dp[0][j] = true;
+            }
+        }
+
+        for (int i = 1; i <= len1; ++i) {
+            for (int j = 1; j <= len2; ++j) {
+                bool fromS1 = (s1[i - 1] == s3[i + j - 1] && dp[i - 1][j]);
+                bool fromS2 = (s2[j - 1] == s3[i + j - 1] && dp[i][j - 1]);
+                dp[i][j] = fromS1 || fromS2;
+            }
+        }
+
+        return dp[len1][len2];
+    }
+};
