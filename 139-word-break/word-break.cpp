60e0711a6e641bb5439c6ab5a0430a7d7f265f88
Allah Wasaya <136077480+wasaya33@users.noreply.github.com>
2025-10-31 15:18:15 +0500

    Time: 11 ms (47.77%) | Memory: 16.1 MB (38.11%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    bool wordBreak(const string& s, vector<string>& wordDict) {
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+        int n = s.size();
+        vector<bool> dp(n + 1, false);
+        dp[0] = true;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (dp[j] && wordSet.count(s.substr(j, i - j))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
