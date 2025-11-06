9a3b67eca1bf1716bd760d7a9c747712589dd856
Allah Wasaya <136077480+wasaya33@users.noreply.github.com>
2025-11-06 20:52:47 +0500

    Time: 35 ms (86.51%) | Memory: 26 MB (58.59%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.cpp
+++ 187-repeated-dna-sequences/repeated-dna-sequences.cpp
@@ -3,12 +3,14 @@ public:
     vector<string> findRepeatedDnaSequences(string s) {
         int n = s.size();
         if (n < 10) return {};
-        unordered_map<string, int> count;
+        unordered_map<string,int> count;
         vector<string> result;
         for (int i = 0; i + 10 <= n; ++i) {
             string sub = s.substr(i, 10);
             count[sub]++;
-            if (count[sub] == 2) result.push_back(sub);
+            if (count[sub] == 2) {
+                result.push_back(sub);
+            }
         }
         return result;
     }
