c528175d8dbd95d76e62dca731ffda07634b2b84
Allah Wasaya <136077480+wasaya33@users.noreply.github.com>
2025-12-10 18:58:30 +0500

    Time: 0 ms (100.00%) | Memory: 92.7 MB (90.50%) - LeetSync

---
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countPermutations(vector<int>& complexity) {
+        const long long MOD = 1000000007;
+        int n = complexity.size();
+        for (int i = 1; i < n; ++i) {
+            if (complexity[i] <= complexity[0]) return 0;
+        }
+        long long ans = 1;
+        for (int i = 1; i < n; ++i) {
+            ans = ans * i % MOD;
+        }
+        return (int)ans;
+    }
+};
