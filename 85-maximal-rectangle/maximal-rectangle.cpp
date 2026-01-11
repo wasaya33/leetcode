2565f4f6aa20c563155e48a3d125088e76344a16
Allah Wasaya <136077480+wasaya33@users.noreply.github.com>
2026-01-11 21:38:14 +0500

    Time: 1 ms (90.35%) | Memory: 17.4 MB (91.45%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,39 +1,37 @@
-#include <vector>
-#include <stack>
-using namespace std;
-
 class Solution {
 public:
+    int largestRectangleArea(vector<int>& h) {
+        vector<int> st;
+        int n = h.size();
+        int best = 0;
+
+        for (int i = 0; i <= n; ++i) {
+            int cur = (i == n ? 0 : h[i]);
+            while (!st.empty() && h[st.back()] > cur) {
+                int height = h[st.back()];
+                st.pop_back();
+                int left = st.empty() ? -1 : st.back();
+                int width = i - left - 1;
+                best = max(best, height * width);
+            }
+            st.push_back(i);
+        }
+        return best;
+    }
+
     int maximalRectangle(vector<vector<char>>& matrix) {
         if (matrix.empty() || matrix[0].empty()) return 0;
         int m = matrix.size(), n = matrix[0].size();
-        vector<int> height(n, 0);
-        int maxArea = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == '1') height[j]++;
-                else height[j] = 0;
+        vector<int> heights(n, 0);
+        int ans = 0;
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == '1') heights[j]++;
+                else heights[j] = 0;
             }
-            maxArea = max(maxArea, largestRectangleArea(height));
+            ans = max(ans, largestRectangleArea(heights));
         }
-        return maxArea;
-    }
-    
-private:
-    int largestRectangleArea(const vector<int>& heights) {
-        int n = heights.size();
-        stack<int> st;
-        vector<int> h(n + 2, 0);
-        for (int i = 0; i < n; i++) h[i + 1] = heights[i];
-        int area = 0;
-        for (int i = 0; i < n + 2; i++) {
-            while (!st.empty() && h[st.top()] > h[i]) {
-                int height = h[st.top()]; st.pop();
-                int width = i - st.top() - 1;
-                area = max(area, height * width);
-            }
-            st.push(i);
-        }
-        return area;
+        return ans;
     }
 };
